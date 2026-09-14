2feb6bc93541b39a10b6cc6118fdb65ace04a52d
pls455 <karamhahhss123@gmail.com>
2026-09-14 22:35:03 +0300

    Make player queue startup reliable

--- app/src/main/java/com/nova/music/player/PlayerController.kt
+++ app/src/main/java/com/nova/music/player/PlayerController.kt
@@ -7,23 +7,31 @@ import androidx.media3.common.MediaItem
 import androidx.media3.session.MediaController
 import androidx.media3.session.SessionToken
 import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.Executors
 
 class PlayerController(context: Context) {
+    private val executor = Executors.newSingleThreadExecutor()
     private val controllerFuture: ListenableFuture<MediaController> =
         MediaController.Builder(
             context,
             SessionToken(context, ComponentName(context, MusicService::class.java))
         ).buildAsync()
 
-    fun setQueue(uris: List<Uri>) {
+    fun playTrack(uris: List<Uri>, index: Int) {
         controllerFuture.addListener({
-            val controller = controllerFuture.get()
-            controller.setMediaItems(uris.map { MediaItem.fromUri(it) })
-            controller.prepare()
-        }, { it.run() })
+            runCatching {
+                val controller = controllerFuture.get()
+                controller.setMediaItems(uris.map { MediaItem.fromUri(it) }, index, 0L)
+                controller.prepare()
+                controller.play()
+            }
+        }, executor)
     }
 
-    fun play() = controllerFuture.addListener({ controllerFuture.get().play() }, { it.run() })
-    fun pause() = controllerFuture.addListener({ controllerFuture.get().pause() }, { it.run() })
-    fun release() = MediaController.releaseFuture(controllerFuture)
+    fun play() = controllerFuture.addListener({ runCatching { controllerFuture.get().play() } }, executor)
+    fun pause() = controllerFuture.addListener({ runCatching { controllerFuture.get().pause() } }, executor)
+    fun release() {
+        MediaController.releaseFuture(controllerFuture)
+        executor.shutdown()
+    }
 }
